7b5c8f452b0089f926225443eeb5fdaf74a0c4d7
Juan Cazala <cazala@mail.sfsu.edu>
2015-11-18 14:05:09 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var q = require('q');
+
+var cache = {
+  data: {}
+}
+
+cache.has = function(key) {
+  return typeof cache.data[key] != 'undefined';
+}
+cache.set = function(key, val) {
+  this.data[key] = val;
+}
+
+cache.get = function(key) {
+  var defer = q.defer();
+  defer.resolve(cache.data[key]);
+  return defer.promise;
+}
+
+cache.remove = function(key) {
+  delete this.data[key];
+  return q.defer().promise;
+}
+
+cache.cache = function(key, callback, expiresInMs) {
+  var res;
+  if(cache.has(key) && expiresInMs !== -1) {
+    res = cache.get(key);
+  } else {
+    res = callback().then(function(data) {
+      cache.set(key, data);
+      return data;
+    });
+  }
+  if(expiresInMs && expiresInMs > 0) {
+    setTimeout(function() {
+      cache.remove(key);
+    }, expiresInMs);
+  }
+  return res;
+}
+
+module.exports = cache;
